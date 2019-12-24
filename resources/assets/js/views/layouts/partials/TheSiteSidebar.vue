89f813156702b6c00fbdc5367d6100b72ce33ad7
Andres Mejia <andres.mejia@pfstech.es>
2019-12-24 13:29:04 +0100

    update translations

--- resources/assets/js/views/layouts/partials/TheSiteSidebar.vue
+++ resources/assets/js/views/layouts/partials/TheSiteSidebar.vue
@@ -37,7 +37,7 @@ export default {
           },
           {
             title: 'navigation.estimates',
-            icon: 'business',
+            icon: 'briefcase',
             route: '/admin/estimates'
           },
           {
